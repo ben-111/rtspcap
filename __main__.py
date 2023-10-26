2b4cf143be30b212216adbec42ad111fdd78df24
Suvid Ernesto <suvid@Sec21.idf>
2023-10-26 16:35:09 +0300

    Support only video

--- __main__.py
+++ __main__.py
@@ -5,7 +5,7 @@ from rtsp_decoder.parse_rtsp import RTSPDataExtractor
 from rtsp_decoder.sdp_to_av import translate_sdp_to_av_codec
 from rtsp_decoder.rtp_decoder import RTPDecoder
 
-from typing import Optional, Dict
+from typing import Dict
 
 def get_sdp_media_from_track_id(sdp_data, track_id: str):
     for media in sdp_data['media']:
@@ -14,32 +14,36 @@ def get_sdp_media_from_track_id(sdp_data, track_id: str):
     raise KeyError('No such track ID in SDP provided')
 
 def get_media_format_specific_config(sdp_media) -> Dict[str, str]:
+    config_dict: Dict[str, str] = dict()
     if 'fmtp' in sdp_media and len(sdp_media['fmtp']) > 0:
         fmtp_data = sdp_media['fmtp'][0]
         if 'config' in fmtp_data:
             config = fmtp_data['config']
             parameters = config.split('; ')
-            config_dict = dict()
             for parameter in parameters:
                 key, value = parameter.split('=')
                 config_dict[key] = value
-            return config_dict
+    if 'type' in sdp_media and sdp_media['type'] == 'audio':
+        if 'rtp' in sdp_media and len(sdp_media['rtp']) > 0 and 'encoding' in sdp_media['rtp'][0]:
+            config_dict['channels'] = sdp_media['rtp'][0]['encoding']
+    return config_dict
 
-def main(pcap_path: str) -> None:
-    rtsp_data = RTSPDataExtractor(pcap_path)
-    rtp_decoder = RTPDecoder('output.mp4')
+def main(input_path: str, output_path: str) -> None:
+    rtsp_data = RTSPDataExtractor(input_path)
+    rtp_decoder = RTPDecoder(output_path)
     for track_id, track in rtsp_data.tracks.items():
         sdp_media = get_sdp_media_from_track_id(rtsp_data.sdp, track_id)
         codec = translate_sdp_to_av_codec(sdp_media['rtp'][0]['codec'])
         rate = sdp_media['rtp'][0]['rate']
         config = get_media_format_specific_config(sdp_media)
-        rtp_capture = FileCapture(pcap_path, display_filter=f'rtp and ip.src == {track.server_ip} and udp.srcport == {track.server_port} and udp.dstport == {track.client_port}')
+        rtp_capture = FileCapture(input_path, display_filter=f'rtp and ip.src == {track.server_ip} and udp.srcport == {track.server_port} and udp.dstport == {track.client_port}')
         rtp_decoder.decode_stream(rtp_capture, config, codec, rate)
     rtp_decoder.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='RTSP server from pcap')
-    parser.add_argument('path', help='Path to pcap with RTSP and RTP data')
+    parser.add_argument('input', help='Path to pcap with RTSP and RTP data')
+    parser.add_argument('output', help='Path to output file')
     args = parser.parse_args()
 
-    main(args.path)
+    main(args.input, args.output)
--- rtp_decoder.py
+++ rtp_decoder.py
@@ -4,42 +4,49 @@ from pyshark.packet.packet import Packet
 
 from typing import Dict, Optional
 
+import logging
+
 class RTPDecoder:
+    MAX_OUT_OF_ORDER_PACKETS = 20
     def __init__(self, output_path: str):
         self.container = av.open(output_path, 'w')
+        self.logger = logging.getLogger(self.__name__)
 
-    def decode_stream(self, rtp_capture: FileCapture, config: Optional[Dict[str, str]], codec: str, rate: int):
+    def decode_stream(self, rtp_capture: FileCapture, config: Dict[str, str], codec: str, rate: int):
         """Assume rtp_capture is filtered so that all RTP packets we see are from the same stream"""
-        out_of_order_packets: Dict[int, Packet] = dict()
-        expected_seq = None
-        while True:
-            packet = rtp_capture.next()
-            if 'RTP' in packet:
-                expected_seq = int(packet['RTP'].seq) + 1
-                break
-
-        assert expected_seq is not None, "RTP stream not found"
-        
         input_codec_ctx = av.codec.CodecContext.create(codec, 'r')
         if input_codec_ctx.type == 'video':
             stream = self.container.add_stream('h264', rate=30)
-        elif input_codec_ctx.type == 'audio':
-            stream = self.container.add_stream('aac')
-        else:
-            raise ValueError('Unsupported stream type')
-        
-        if config is not None:
-            input_codec_ctx.options = config
             if 'config' in config:
-                input_codec_ctx.parse(bytes.fromhex(config['config']))
+                input_codec_ctx.extradata = bytes.fromhex(config['config'])
+        # TODO: support audio
+        # elif input_codec_ctx.type == 'audio':
+        #     stream = self.container.add_stream('aac')
+        #     input_codec_ctx.rate = rate
+        #     input_codec_ctx.channels = config['channels']
+        else:
+            self.logger.warning(f'Unsupported stream type: {input_codec_ctx.type}, with codec: {codec}, skipping')
+            return
 
-        for packet in filter(lambda x: 'RTP' in x, rtp_capture):
+        out_of_order_packets: Dict[int, Packet] = dict()
+        expected_seq = None
+        rtp_stream = filter(lambda x: 'RTP' in x, rtp_capture)
+        try:
+            expected_seq = int(next(rtp_stream)['RTP'].seq) + 1
+        except StopIteration:
+            raise ValueError('RTP stream not found')
+
+        for packet in rtp_stream:
             seq = int(packet['RTP'].seq)
             if seq != expected_seq:
                 if seq in out_of_order_packets:
                     packet = out_of_order_packets.pop(seq)
                 else:
-                    out_of_order_packets[seq] = packet
+                    if len(out_of_order_packets) > self.MAX_OUT_OF_ORDER_PACKETS:
+                        # We likely lost a packet
+                        expected_seq += 1
+                    else:
+                        out_of_order_packets[seq] = packet
                     continue
 
             chunk = bytes.fromhex(packet['RTP'].payload.raw_value)
