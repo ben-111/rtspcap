124d78c95385d0da4c1ecbd96a34f3f7f522d31e
Ben Lavi <benlavi111@gmail.com>
2023-11-08 17:25:44 +0200

    Add reassembler modes

--- rtsp_decoder/reassembler.py
+++ rtsp_decoder/reassembler.py
@@ -1,6 +1,6 @@
 import logging
 
-from typing import TypeVar, Generic, Dict, Optional, List, Iterator, Tuple
+from typing import TypeVar, Generic, Dict, Optional, List, Iterator, Tuple, Literal
 
 T = TypeVar("T")
 
@@ -8,12 +8,19 @@ T = TypeVar("T")
 class Reassembler(Generic[T]):
     """
     This class is a generic best-effort reassembler.
+
+    If operating in packet mode, the sequence number is incremented by one for each packet.
+    If operating in data mode, the sequence number is incremented by the length of the data.
     """
 
-    def __init__(self, seq_bit_size: int, max_out_of_order: int):
+    def __init__(
+        self, seq_bit_size: int, max_out_of_order: int, mode: Literal["packet", "data"]
+    ):
         self.logger = logging.getLogger(__name__)
         self._seq_bit_size = seq_bit_size
         self._max_out_of_order = max_out_of_order
+        assert mode in ("packet", "data")
+        self._mode = mode
         self._out_of_order_packets: Dict[int, T] = {}
         self._expected_seq: Optional[int] = None
         self._output_queue: List[Tuple[Optional[T], bool]] = []
@@ -24,8 +31,13 @@ class Reassembler(Generic[T]):
             packet_and_skipped = self._output_queue.pop(0)
             yield packet_and_skipped
 
-    def _increment_expected_seq(self) -> None:
-        self._expected_seq += 1
+    def _increment_expected_seq(self, packet: T) -> None:
+        if self._mode == "packet":
+            seq_size = 1
+        elif self._mode == "data":
+            seq_size = len(packet)
+
+        self._expected_seq += seq_size
         self._expected_seq %= 1 << self._seq_bit_size
 
     def process(self, packet: Optional[T], seq: int = -1) -> None:
@@ -54,7 +66,7 @@ class Reassembler(Generic[T]):
                 if skipped:
                     self._expected_seq = earliest_packet_seq
 
-                self._increment_expected_seq()
+                self._increment_expected_seq(packet)
 
             self._output_queue.append((None, False))
             return
@@ -69,25 +81,20 @@ class Reassembler(Generic[T]):
             self.logger.debug(
                 f"Could not find packet with sequence number {self._expected_seq}; Likely packet loss"
             )
-            if self._out_of_order_packets:
-                self._expected_seq = min(self._out_of_order_packets.keys())
-                self._output_queue.append(
-                    (self._out_of_order_packets.pop(self._expected_seq), True)
-                )
-                while True:
-                    self._increment_expected_seq()
-                    if self._expected_seq not in self._out_of_order_packets:
-                        break
 
-                    self._output_queue.append(
-                        (self._out_of_order_packets.pop(self._expected_seq), False)
-                    )
+            self._expected_seq = min(self._out_of_order_packets.keys())
+            next_packet = self._out_of_order_packets.pop(self._expected_seq)
+            self._output_queue.append((next_packet, True))
+            while True:
+                self._increment_expected_seq(next_packet)
+                if self._expected_seq not in self._out_of_order_packets:
+                    break
 
-            else:
-                self._increment_expected_seq()
+                next_packet = self._out_of_order_packets.pop(self._expected_seq)
+                self._output_queue.append((next_packet, False))
 
             return
 
         # Else, put the packet in the output queue
         self._output_queue.append((packet, False))
-        self._increment_expected_seq()
+        self._increment_expected_seq(packet)
